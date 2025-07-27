fa96f544c89ec75db05df23a2fe936d64a50ca40
DEO PRAKASH <deo.prakash@ssipmt.com>
2025-07-28 00:47:07 +0530

    Array

---
+++ array.js
@@ -0,0 +1,25 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr);
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(8));
+
+
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1)
+
+// const myn2 = myArr.splice(1, 3)
+// console.log(myn2)
+
+const city1 = ['Delhi', 'Jaipur', 'Bhopal'];
+const city2 = ['Mumbai', 'Surat', 'Indore'];
+
+const city_names = [...city1, ...city2]
+console.log(city_names);
+
+
